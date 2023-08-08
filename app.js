0be107f33299d229453ee273ba9534c94aee6f1f
josephhansen-bcss <141339577+josephhansen-bcss@users.noreply.github.com>
2023-08-08 15:56:12 -0500

    npm i hello world

---
+++ app.js
@@ -0,0 +1,8 @@
+//node.js Hello World
+var http = require('http');
+var port = process.env.PORT || 8668;
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World\n');
+}).listen(port);
+console.log("Server running at http://localhost:%d", port);
