1e26437f9c7014f11290b65e000997f7ff8bc310
josephhansen-bcss <141339577+josephhansen-bcss@users.noreply.github.com>
2023-08-10 13:06:21 -0500

    toast styling

--- overlay/functions/process/accept.js
+++ overlay/functions/process/accept.js
@@ -7,6 +7,7 @@ import {Clear} from '/overlay/functions/tool'
 function postToPython(){
     let url = window.location.protocol + "//" + window.location.hostname + ":5000"
     $('#loading-toast').css('width', 'auto')
+    $('#loading-toast').css('opacity', '.6')
     $('#loading-toast-p').css('opacity', '1')
     $.ajax({
         url: url,
@@ -21,6 +22,7 @@ function postToPython(){
 function callback(data){
     Clear()
     $('#loading-toast').css('width', '0vw')
+    $('#loading-toast').css('opacity', '0')
     $('#loading-toast-p').css('opacity', '0')
 }
 
