8674e9fa1ac19a89497b7f9f20f367bbc1cbc346
josephhansen-bcss <141339577+josephhansen-bcss@users.noreply.github.com>
2023-08-11 08:53:26 -0500

    minify static files (~24-35% savings)

--- app.js
+++ app.js
@@ -4,73 +4,73 @@ const app = express()
 app.get('/overlay/css', function (req, res) {
     //send file as MIME CSS type
     res.type('text/css')
-    res.sendFile('/overlay/overlay.css', { root: '.'})
+    res.sendFile('/overlay/overlay.min.css', { root: '.'})
 })
 
 app.get('/overlay/functions/grabber', function (req, res) {
     //send file as MIME JS type
     res.type('text/javascript')
-    res.sendFile('/overlay/functions/grabber.js', { root: '.'})
+    res.sendFile('/overlay/functions/grabber.min.js', { root: '.'})
 })
 
 app.get('/overlay/functions/scroll', function (req, res) {
     //send file as MIME JS type
     res.type('text/javascript')
-    res.sendFile('/overlay/functions/scroll.js', { root: '.'})
+    res.sendFile('/overlay/functions/scroll.min.js', { root: '.'})
 })
 
 app.get('/overlay/functions/tool', function (req, res) {
     //send file as MIME JS type
     res.type('text/javascript')
-    res.sendFile('/overlay/functions/tool.js', { root: '.'})
+    res.sendFile('/overlay/functions/tool.min.js', { root: '.'})
 })
 
 app.get('/tool/brush', function (req, res) {
     //send file as MIME JS type
     res.type('text/javascript')
-    res.sendFile('/overlay/functions/tools/brush.js', { root: '.'})
+    res.sendFile('/overlay/functions/tools/brush.min.js', { root: '.'})
 })
 
 app.get('/tool/circle', function (req, res) {
     //send file as MIME JS type
     res.type('text/javascript')
-    res.sendFile('/overlay/functions/tools/circle.js', { root: '.'})
+    res.sendFile('/overlay/functions/tools/circle.min.js', { root: '.'})
 })
 
 app.get('/tool/rectangle', function (req, res) {
     //send file as MIME JS type
     res.type('text/javascript')
-    res.sendFile('/overlay/functions/tools/rectangle.js', { root: '.'})
+    res.sendFile('/overlay/functions/tools/rectangle.min.js', { root: '.'})
 })
 
 app.get('/tool/blur', function (req, res) {
     //send file as MIME JS type
     res.type('text/javascript')
-    res.sendFile('/overlay/functions/tools/blur.js', { root: '.'})
+    res.sendFile('/overlay/functions/tools/blur.min.js', { root: '.'})
 })
 
 app.get('/tool/text', function (req, res) {
     //send file as MIME JS type
     res.type('text/javascript')
-    res.sendFile('/overlay/functions/tools/text.js', { root: '.'})
+    res.sendFile('/overlay/functions/tools/text.min.js', { root: '.'})
 })
 
 app.get('/tool/comment', function (req, res) {
     //send file as MIME JS type
     res.type('text/javascript')
-    res.sendFile('/overlay/functions/tools/comment.js', { root: '.'})
+    res.sendFile('/overlay/functions/tools/comment.min.js', { root: '.'})
 })
 
 app.get('/tool/accept', function (req, res) {
     //send file as MIME JS type
     res.type('text/javascript')
-    res.sendFile('/overlay/functions/process/accept.js', { root: '.'})
+    res.sendFile('/overlay/functions/process/accept.min.js', { root: '.'})
 })
 
 app.get('/tool/reject', function (req, res) {
     //send file as MIME JS type
     res.type('text/javascript')
-    res.sendFile('/overlay/functions/process/accept.js', { root: '.'})
+    res.sendFile('/overlay/functions/process/accept.min.js', { root: '.'})
 })
 
 app.get('/overlay', function (req, res) {
